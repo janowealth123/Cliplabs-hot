eef82b0ff3bd8446ccdf504f78ef07e0c7fb2675
janowealth123 <janospadafora@gmail.com>
2025-01-28 08:11:17 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GlowLanding from './GlowLanding';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlowLanding />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
