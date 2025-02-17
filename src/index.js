2346640e3b242eb0c9a7da70c4622ee8fa1c5950
janowealth123 <janospadafora@gmail.com>
2025-02-17 18:25:09 +0500

    add routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import GlowLanding from './GlowLanding';
 import WebinarPage from './webinar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WebinarPage/>,
+  },
+  {
+    path:"/blank",
+    element:<GlowLanding />
+  }
+]);
 ReactDOM.render(
   <React.StrictMode>
-    {/* <GlowLanding /> */}
-     <WebinarPage/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
